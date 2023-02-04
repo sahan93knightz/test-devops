42f95509513d3cb25c52c14faa7c2ef39323a4c1
Sahan Ranasinghe <sahan93knightz@gmail.com>
2023-02-04 17:23:12 +0530

    create endpoint to return host IP and port

---
+++ src/main/java/com/example/demodevops/controller/MainController.java
@@ -0,0 +1,22 @@
+package com.example.demodevops.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+
+@RestController
+public class MainController {
+
+    @Value("${server.port}")
+    private int serverPort;
+
+    @GetMapping("/")
+    public ResponseEntity<String> getHello() {
+        final String hostname = InetAddress.getLoopbackAddress().getHostAddress();
+
+        return ResponseEntity.ok("Hello from " + hostname + ":" + serverPort);
+    }
+}
